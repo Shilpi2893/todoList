7aba6104da71c990ab99d97180370acfc2ffc8eb
Shilpi2893 <78659495+Shilpi2893@users.noreply.github.com>
2023-08-05 18:02:31 +0100

    adding items to list

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+///// Idhar hum buttons ko listen kar rahe hai
+
 var addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addToDoItem);
 function addToDoItem() {
-    alert("Add button clicked");
+    // alert("Add button clicked");
+    var itemText = toDoEntryBox.value;
+    newToDoItem(itemText, false);
 }
 
 var clearCompleted = document.getElementById("clear-completed-button");
@@ -20,4 +24,30 @@ var saveButton = document.getElementById("save-button");
 saveButton.addEventListener("click", saveList);
 function saveList() {
     alert("Add Save button");
+}
+
+//// Idhar hum list add karenge input se
+
+var toDoEntryBox = document.getElementById("todo-entry-box");
+var toDoList = document.getElementById("todo-list");
+
+function newToDoItem( itemNext, completed ) {
+    var toDoItem = document.createElement("li");
+    var toDoText = document.createTextNode(itemNext);
+    toDoItem.appendChild(toDoText);
+
+    if( completed ) {
+        toDoItem.classList.add("completed");
+    }
+
+    toDoList.appendChild(toDoItem);
+    toDoItem.addEventListener("dblclick", toggleToDoItemState);
+}
+
+function toggleToDoItemState() {
+    if( this.classList.contains("completed") ) {
+        this.classList.remove("completed");
+    } else {
+        this.classList.add("completed");
+    }
 }
